eac737929dbe7f2169e6a907e0536a60c8a0f379
yakibuta888 <yakibuta888@gmail.com>
2024-03-14 13:03:39 +0900

    voi service, repositoryの作成

--- backend/src/domain/services/volume_oi_service.py
+++ backend/src/domain/services/volume_oi_service.py
@@ -1,11 +1,28 @@
 # src/domain/services/volume_oi_service.py
 import pandas as pd
-from typing import Any, Dict
-from sqlalchemy.orm import Session
+from typing import NamedTuple
 
 from src.domain.entities.volume_oi_entity import VolumeOIEntity
 from src.domain.repositories.volume_oi_repository import VolumeOIRepository
 from src.settings import logger
+
+
+_VOINamedTuple = NamedTuple('_VOINamedTuple', [
+    ('trade_date', str),
+    ('month', str),
+    ('globex', int),
+    ('open_outcry', int),
+    ('clear_port', int),
+    ('total_volume', int),
+    ('block_trades', int),
+    ('efp', int),
+    ('efr', int),
+    ('tas', int),
+    ('deliveries', int),
+    ('at_close', int),
+    ('change', int)
+])
+
 
 class VolumeOIService:
     def __init__(self, volume_oi_repository: VolumeOIRepository):
@@ -15,43 +32,40 @@ class VolumeOIService:
         # DataFrameの各列の型を変換する
         df = self._transform_dataframe_types(df)
 
-        for _, row in df.iterrows():
+        for row in df.itertuples(index=False):
             try:
                 volume_oi_entity = self._row_to_entity(row, asset_id)
                 self.volume_oi_repository.create(volume_oi_entity)
             except ValueError as e:
-                logger.error(f"Validation error for row {row.to_dict()}: {e}")
+                logger.error(f"Validation error for row {row}: {e}")
                 raise e
             except Exception as e:
-                logger.error(f"Error saving volume and open interest data for row {row.to_dict()}: {e}")
+                logger.error(f"Error saving volume and open interest data for row {row}: {e}")
                 raise e
 
-        logger.info(f"Volume and open interest data for asset {asset_id} saved successfully.")
+        logger.info(f"Volume and open interest data for asset {asset_id} - {volume_oi_entity.trade_date} saved successfully.")
 
     def _transform_dataframe_types(self, df: pd.DataFrame) -> pd.DataFrame:
-        # 必要に応じて型変換を行う
-        df['trade_date'] = pd.to_datetime(df['trade_date']).dt.date
-        # 他の数値列に対して適切な型変換を行う
         numeric_columns = ['globex', 'open_outcry', 'clear_port', 'total_volume', 'block_trades', 'efp', 'efr', 'tas', 'deliveries', 'at_close', 'change']
         for col in numeric_columns:
             df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype(int)
         return df
 
-    def _row_to_entity(self, row: pd.Series, asset_id: int) -> VolumeOIEntity:
+    def _row_to_entity(self, row: _VOINamedTuple , asset_id: int) -> VolumeOIEntity:
         # DataFrameの行からEntityを生成
         return VolumeOIEntity.new_entity(
             asset_id=asset_id,
-            trade_date=row['trade_date'],
-            month=row['month'],
-            globex=row['globex'],
-            open_outcry=row['open_outcry'],
-            clear_port=row['clear_port'],
-            total_volume=row['total_volume'],
-            block_trades=row['block_trades'],
-            efp=row['efp'],
-            efr=row['efr'],
-            tas=row['tas'],
-            deliveries=row['deliveries'],
-            at_close=row['at_close'],
-            change=row['change']
+            trade_date=row.trade_date,
+            month=row.month,
+            globex=row.globex,
+            open_outcry=row.open_outcry,
+            clear_port=row.clear_port,
+            total_volume=row.total_volume,
+            block_trades=row.block_trades,
+            efp=row.efp,
+            efr=row.efr,
+            tas=row.tas,
+            deliveries=row.deliveries,
+            at_close=row.at_close,
+            change=row.change
         )
---
+++ backend/src/infrastructure/repositories/volume_oi_repository_mysql.py
@@ -0,0 +1,37 @@
+# src/infrastructure/repositories/volume_oi_repository_mysql.py
+from sqlalchemy.orm import Session
+
+from src.domain.entities.volume_oi_entity import VolumeOIEntity
+from src.domain.repositories.volume_oi_repository import VolumeOIRepository
+from src.infrastructure.database.models import VolumeOI as VolumeOIModel
+from src.settings import logger
+
+class VolumeOIRepositoryMysql(VolumeOIRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create(self, volume_oi_entity: VolumeOIEntity) -> VolumeOIModel:
+        try:
+            volume_oi_model = VolumeOIModel(
+                asset_id=volume_oi_entity.asset_id,
+                trade_date=volume_oi_entity.trade_date.to_date(),
+                month=volume_oi_entity.month.to_db_format(),
+                globex=volume_oi_entity.globex,
+                open_outcry=volume_oi_entity.open_outcry,
+                clear_port=volume_oi_entity.clear_port,
+                total_volume=volume_oi_entity.total_volume,
+                block_trades=volume_oi_entity.block_trades,
+                efp=volume_oi_entity.efp,
+                efr=volume_oi_entity.efr,
+                tas=volume_oi_entity.tas,
+                deliveries=volume_oi_entity.deliveries,
+                at_close=volume_oi_entity.at_close,
+                change=volume_oi_entity.change
+            )
+            self.session.add(volume_oi_model)
+            self.session.commit()
+            return volume_oi_model
+        except Exception as e:
+            self.session.rollback()
+            logger.error(f"Error saving volume and open interest data: {e}")
+            raise e
