d032af6049390005790fe98fa9902b4b82c7ee8a
yakibuta888 <yakibuta888@gmail.com>
2024-03-28 06:36:08 +0900

    GitHubActions用にloggerのファイルハンドラを削除

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -13,6 +13,14 @@ LOG_CONFIG_PATH: str = os.path.normpath(os.path.join(CWD, "log/log_config.yaml")
 def setup_logging() -> None:
     with open(LOG_CONFIG_PATH, 'r', encoding='utf-8') as f:
         log_config = yaml.safe_load(f)
+
+        # GitHub Actionsや他のCI環境での実行を検出
+        if os.environ.get('CI'):  # CI環境であればTrue
+            # ファイルハンドラを削除し、コンソールハンドラのみを使用
+            log_config['handlers'].pop('file', None)
+            for logger in log_config['loggers'].values():
+                logger['handlers'] = [handler for handler in logger['handlers'] if handler != 'file']
+
         logging.config.dictConfig(log_config)  # type: ignore
 
 
