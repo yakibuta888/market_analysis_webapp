09298d24ecb76327585c06ef9d0029066717d897
yakibuta888 <yakibuta888@gmail.com>
2024-05-05 14:09:19 +0900

    viewModelsリネーム

---
+++ frontend/src/viewModels/DashboardViewModel.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { AnyAction } from 'redux';
+
+import { fetchUser } from '../store/modules/user';
+import { RootState } from '../store';
+
+
+export const useDashboardViewModel = () => {
+  const user = useSelector((state: RootState) => state.user.userData);
+  const dispatch = useDispatch<ThunkDispatch<RootState, unknown, AnyAction>>();
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(fetchUser());
+    }
+  }, [dispatch, user]);
+
+  // ViewModelがダッシュボード固有の状態を持つ場合の例
+  const [selectedChart, setSelectedChart] = useState<number | null>(null);
+
+  const selectChart = (chartId: number) => {
+    setSelectedChart(chartId);
+  };
+
+  return { user, selectedChart, selectChart };
+};
---
+++ frontend/src/viewModels/UserViewModel.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { AnyAction } from 'redux';
+
+import { fetchUser } from '../store/modules/user';
+import { RootState } from '../store';
+
+
+export const useUserViewModel = () => {
+  const user = useSelector((state: RootState) => state.user.userData);
+  const loading = useSelector((state: RootState) => state.user.loading);
+  const error = useSelector((state: RootState) => state.user.error);
+  const dispatch = useDispatch<ThunkDispatch<RootState, unknown, AnyAction>>();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  return { user, loading, error };
+};
