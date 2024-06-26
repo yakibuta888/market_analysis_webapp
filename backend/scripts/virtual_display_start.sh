fe2f1a5730da849549be9cd22a1dd995916aeddc
yakibuta888 <yakibuta888@gmail.com>
2024-06-26 13:04:28 +0900

    xvfbの設定修正

--- backend/scripts/virtual_display_start.sh
+++ backend/scripts/virtual_display_start.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 # XVFBの起動
 if ! pgrep -x Xvfb > /dev/null;
 then
-  Xvfb :99 -screen 0 1920x1080x24 &
+  Xvfb :99 -screen 0 1920x1080x24 -nolisten unix &
   echo "Xvfb started on display :99."
 else
   echo "Xvfb is already running."
